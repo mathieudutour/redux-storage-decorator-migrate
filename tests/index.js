09697e5e2fed7925efadc51e5d6496a8ebbf5444
Mathieu Dutour <mathieu@dutour.me>
2016-03-21 12:38:20 +0000

    use @mathieudutour/js-fatigue

--- tests/index.js
+++ tests/index.js
@@ -2,14 +2,13 @@ import test from 'ava'
 import sinon from 'sinon'
 import 'sinon-as-promised'
 
-import 'babel-register'
 import migrate from '../src/'
 
 function identityStub (arg) {
-  return new Promise(resolve => resolve(arg))
+  return new Promise((resolve) => resolve(arg))
 }
 
-test('should proxy save to engine.save', async t => {
+test('should proxy save to engine.save', async (t) => {
   t.plan(1)
 
   const save = sinon.spy()
@@ -20,7 +19,7 @@ test('should proxy save to engine.save', async t => {
   t.true(save.calledOnce)
 })
 
-test('should proxy save to engine.save without mutating the state', async t => {
+test('should proxy save to engine.save without mutating the state', async (t) => {
   t.plan(2)
 
   const save = sinon.spy()
@@ -33,7 +32,7 @@ test('should proxy save to engine.save without mutating the state', async t => {
   t.same(state, {})
 })
 
-test('should save the current version at the default key', async t => {
+test('should save the current version at the default key', async (t) => {
   t.plan(1)
 
   const save = identityStub
@@ -44,7 +43,7 @@ test('should save the current version at the default key', async t => {
   t.same(state, { 'redux-storage-decorators-migrate-version': 42 })
 })
 
-test('should save the current version at the custom key', async t => {
+test('should save the current version at the custom key', async (t) => {
   t.plan(1)
 
   const save = identityStub
@@ -55,7 +54,7 @@ test('should save the current version at the custom key', async t => {
   t.same(state, { 'custom-key': 42 })
 })
 
-test('should proxy load to engine.load without migrations', async t => {
+test('should proxy load to engine.load without migrations', async (t) => {
   t.plan(1)
 
   const load = sinon.stub().resolves({})
@@ -66,7 +65,7 @@ test('should proxy load to engine.load without migrations', async t => {
   t.same(state, {})
 })
 
-test('should remove the key from the loaded state with default key', async t => {
+test('should remove the key from the loaded state with default key', async (t) => {
   t.plan(1)
 
   const load = sinon.stub().resolves({ 'redux-storage-decorators-migrate-version': 42 })
@@ -77,7 +76,7 @@ test('should remove the key from the loaded state with default key', async t => 
   t.same(state, {})
 })
 
-test('should remove the key from the loaded state with custom key', async t => {
+test('should remove the key from the loaded state with custom key', async (t) => {
   t.plan(1)
 
   const load = sinon.stub().resolves({ 'custom-key': 42 })
@@ -88,7 +87,7 @@ test('should remove the key from the loaded state with custom key', async t => {
   t.same(state, {})
 })
 
-test('should applied migrations to the loaded state when the current version is higher', async t => {
+test('should applied migrations to the loaded state when the current version is higher', async (t) => {
   t.plan(1)
 
   const load = sinon.stub().resolves({
@@ -109,7 +108,7 @@ test('should applied migrations to the loaded state when the current version is 
   t.same(state, { key: 2 })
 })
 
-test('should not applied migrations to the loaded state when the current version is the same', async t => {
+test('should not applied migrations to the loaded state when the current version is the same', async (t) => {
   t.plan(1)
 
   const load = sinon.stub().resolves({
@@ -130,7 +129,7 @@ test('should not applied migrations to the loaded state when the current version
   t.same(state, { key: 1 })
 })
 
-test('should not applied all migrations to the loaded state between the current version and the loaded version', async t => {
+test('should not applied all migrations to the loaded state between the current version and the loaded version', async (t) => {
   t.plan(1)
 
   const load = sinon.stub().resolves({
